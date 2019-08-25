421d31b8fefc6f9c8404a51b43ee9a9738f6cea0
MikeStrenk <mikestrenk@gmail.com>
2019-08-25 14:23:42 -0500

    updated setup config

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mailroom-pkg",
+    name="mailroom",
     version="0.0.1",
     author="Michael Strenk",
-    author_email="mikestrenk@gmail.com",
     description="Toolkit to generate & distribute html email reports",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/MikeStrenk/mailroom",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        'Development Status :: 3 - Alpha',
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6"
     ],
 )
