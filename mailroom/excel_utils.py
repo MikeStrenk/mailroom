dc0962cb591a15e68d240a04ef66af5992baf3e0
MikeStrenk <mikestrenk@gmail.com>
2020-02-15 11:42:12 -0600

    corrected imports

--- mailroom/excel_utils.py
+++ mailroom/excel_utils.py
@@ -1,11 +1,17 @@
-def output_excel(df, wkng_folder, filename):
+import pandas as pd
+
+
+def format_excel():
+    pass
+
+
+def output_excel(df, wkng_folder, filename, sheet_name="sheet1"):
     """Renders and saves an excel file from a dataframe, returns full filepath"""
     full_filepath = wkng_folder + filename
 
     writer = pd.ExcelWriter(full_filepath, engine="xlsxwriter")
 
-    df.to_excel(writer, sheet_name="team_data", index=False)
-    master_df.to_excel(writer, sheet_name="raw_data", index=False)
+    df.to_excel(writer, sheet_name=sheet_name, index=False)
 
     df_col_names = [{"header": col_name} for col_name in df.columns.values.tolist()]
 
