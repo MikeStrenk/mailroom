3202c0b57412f024a27097099f0e0a9532b3edc5
Michael <michael@Michaels-MacBook-Pro.local>
2020-03-22 11:37:06 -0500

    updated import statements

--- mailroom/__init__.py
+++ mailroom/__init__.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 # flake8: noqa
 
 from mailroom.report_utils import Email, Chart
 from mailroom.email_client import exchange
 
-=======
-from mailroom.email import Email
-from mailroom.chart import Chart
-
->>>>>>> 69e38bf1d990cf64c4e5f2ab38a9566866554408
 __version__ = "0.0.1"
